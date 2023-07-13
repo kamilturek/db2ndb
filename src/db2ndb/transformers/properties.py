1533a9556cd3afa5d044f9a670b161dbe4e91394
Kamil Turek <kamil.turek@hotmail.com>
2023-07-13 23:06:30 +0200

    Create simple properties transformers

--- src/db2ndb/transformers/properties.py
+++ src/db2ndb/transformers/properties.py
@@ -2,14 +2,69 @@ import libcst as cst
 import libcst.matchers as m
 from libcst.codemod import ContextAwareTransformer
 
-__all__ = ("ReplaceBlobProperty",)
+__all__ = (
+    "ReplaceBlobProperty",
+    "ReplaceBooleanProperty",
+    "ReplaceCategoryProperty",
+    "ReplaceDateProperty",
+    "ReplaceDateTimeProperty",
+    "ReplaceEmailProperty",
+    "ReplaceFloatProperty",
+    "ReplaceGeoPtProperty",
+    "ReplaceIntegerProperty",
+    "ReplaceLinkProperty",
+    "ReplacePhoneNumberProperty",
+    "ReplacePostalAddressProperty",
+    "ReplaceRatingProperty",
+    "ReplaceStringProperty",
+    "ReplaceTextProperty",
+    "ReplaceTimeProperty",
+    "ReplaceUserProperty",
+    "ReplaceBlobReferenceProperty",
+)
 
 
-class ReplaceBlobProperty(ContextAwareTransformer):
-    @m.leave(m.Attribute(value=m.Name(value="db"), attr=m.Name(value="BlobProperty")))
-    def _transform(
-        self, original_node: cst.Attribute, updated_node: cst.Attribute
-    ) -> cst.Attribute:
-        return updated_node.with_changes(
-            value=cst.Name(value="ndb"), attr=cst.Name(value="BlobProperty")
+def ReplaceProperty(before: str, after: str) -> ContextAwareTransformer:
+    before_value, before_attr = before.split(".")
+    after_value, after_attr = after.split(".")
+
+    class _ReplaceProperty(ContextAwareTransformer):
+        @m.leave(
+            m.Attribute(
+                value=m.Name(value=before_value), attr=m.Name(value=before_attr)
+            )
         )
+        def _transform(
+            self, original_node: cst.Attribute, updated_node: cst.Attribute
+        ) -> cst.Attribute:
+            return updated_node.with_changes(
+                value=cst.Name(value=after_value), attr=cst.Name(value=after_attr)
+            )
+
+    return _ReplaceProperty
+
+
+ReplaceBlobProperty = ReplaceProperty("db.BlobProperty", "ndb.BlobProperty")
+ReplaceBooleanProperty = ReplaceProperty("db.BooleanProperty", "ndb.BooleanProperty")
+ReplaceCategoryProperty = ReplaceProperty("db.CategoryProperty", "ndb.StringProperty")
+ReplaceDateProperty = ReplaceProperty("db.DateProperty", "ndb.DateProperty")
+ReplaceDateTimeProperty = ReplaceProperty("db.DateTimeProperty", "ndb.DateTimeProperty")
+ReplaceEmailProperty = ReplaceProperty("db.EmailProperty", "ndb.StringProperty")
+ReplaceFloatProperty = ReplaceProperty("db.FloatProperty", "ndb.FloatProperty")
+ReplaceGeoPtProperty = ReplaceProperty("db.GeoPtProperty", "ndb.GeoPtProperty")
+ReplaceIntegerProperty = ReplaceProperty("db.IntegerProperty", "ndb.IntegerProperty")
+ReplaceLinkProperty = ReplaceProperty("db.LinkProperty", "ndb.StringProperty")
+ReplacePhoneNumberProperty = ReplaceProperty(
+    "db.PhoneNumberProperty", "ndb.StringProperty"
+)
+ReplacePostalAddressProperty = ReplaceProperty(
+    "db.PostalAddressProperty", "ndb.StringProperty"
+)
+ReplaceRatingProperty = ReplaceProperty("db.RatingProperty", "ndb.IntegerProperty")
+ReplaceStringProperty = ReplaceProperty("db.StringProperty", "ndb.StringProperty")
+ReplaceTextProperty = ReplaceProperty("db.TextProperty", "ndb.TextProperty")
+ReplaceTimeProperty = ReplaceProperty("db.TimeProperty", "ndb.TimeProperty")
+ReplaceUserProperty = ReplaceProperty("db.UserProperty", "ndb.UserProperty")
+ReplaceBlobReferenceProperty = ReplaceProperty(
+    "blobstore.BlobReferenceProperty", "ndb.BlobKeyProperty"
+)
--- tests/test_transformers/test_properties.py
+++ tests/test_transformers/test_properties.py
@@ -1,16 +1,62 @@
+import pytest
+
 from db2ndb.main import transform
-from db2ndb.transformers import ReplaceBlobProperty
+from db2ndb.transformers import (
+    ReplaceBlobProperty,
+    ReplaceBlobReferenceProperty,
+    ReplaceBooleanProperty,
+    ReplaceCategoryProperty,
+    ReplaceDateProperty,
+    ReplaceDateTimeProperty,
+    ReplaceEmailProperty,
+    ReplaceFloatProperty,
+    ReplaceGeoPtProperty,
+    ReplaceIntegerProperty,
+    ReplaceLinkProperty,
+    ReplacePhoneNumberProperty,
+    ReplacePostalAddressProperty,
+    ReplaceRatingProperty,
+    ReplaceStringProperty,
+    ReplaceTextProperty,
+    ReplaceTimeProperty,
+    ReplaceUserProperty,
+)
 
 
-def test_replace_blob_property():
-    before = """
-db.BlobProperty()
-"""
-
-    after = """
-ndb.BlobProperty()
-"""
-
-    transformers = [ReplaceBlobProperty]
-
-    assert transform(before, transformers) == after
+@pytest.mark.parametrize(
+    "transformer,before,after",
+    [
+        (ReplaceBlobProperty, "db.BlobProperty()", "ndb.BlobProperty()"),
+        (ReplaceBooleanProperty, "db.BooleanProperty()", "ndb.BooleanProperty()"),
+        (ReplaceCategoryProperty, "db.CategoryProperty()", "ndb.StringProperty()"),
+        (ReplaceDateProperty, "db.DateProperty()", "ndb.DateProperty()"),
+        (ReplaceDateTimeProperty, "db.DateTimeProperty()", "ndb.DateTimeProperty()"),
+        (ReplaceEmailProperty, "db.EmailProperty()", "ndb.StringProperty()"),
+        (ReplaceFloatProperty, "db.FloatProperty()", "ndb.FloatProperty()"),
+        (ReplaceGeoPtProperty, "db.GeoPtProperty()", "ndb.GeoPtProperty()"),
+        (ReplaceIntegerProperty, "db.IntegerProperty()", "ndb.IntegerProperty()"),
+        (ReplaceLinkProperty, "db.LinkProperty()", "ndb.StringProperty()"),
+        (
+            ReplacePhoneNumberProperty,
+            "db.PhoneNumberProperty()",
+            "ndb.StringProperty()",
+        ),
+        (
+            ReplacePostalAddressProperty,
+            "db.PostalAddressProperty()",
+            "ndb.StringProperty()",
+        ),
+        (ReplaceRatingProperty, "db.RatingProperty()", "ndb.IntegerProperty()"),
+        (ReplaceStringProperty, "db.StringProperty()", "ndb.StringProperty()"),
+        (ReplaceTextProperty, "db.TextProperty()", "ndb.TextProperty()"),
+        (ReplaceTimeProperty, "db.TimeProperty()", "ndb.TimeProperty()"),
+        (ReplaceUserProperty, "db.UserProperty()", "ndb.UserProperty()"),
+        (
+            ReplaceBlobReferenceProperty,
+            "blobstore.BlobReferenceProperty()",
+            "ndb.BlobKeyProperty()",
+        ),
+    ],
+)
+def test_properties(transformer, before, after):
+    assert transform(before, [transformer]) == after
